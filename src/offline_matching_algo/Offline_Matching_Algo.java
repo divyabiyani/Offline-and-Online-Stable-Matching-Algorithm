4fb1b79abd80e98d80898e8b3be5e4261485dca4
divyabiyani <divyabiyani26@gmail.com>
2015-11-06 02:50:45 +0530

    All files addes

---
+++ src/offline_matching_algo/Offline_Matching_Algo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package offline_matching_algo;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Dell
+ */
+public class Offline_Matching_Algo {
+      
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        
+        Home hm=new Home();
+        hm.setSize(900,600);
+       
+        hm.setVisible(true); // TODO code application logic here
+    }
+    
+}
